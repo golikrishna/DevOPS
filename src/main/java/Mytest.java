0c8de298f087acc98ee5a72dcfbd7c535f130030
golikrishna <49666896+golikrishna@users.noreply.github.com>
2019-04-30 19:45:07 +0530

    Update Mytest.java

--- src/main/java/Mytest.java
+++ src/main/java/Mytest.java
@@ -1,7 +1,7 @@
 public class Mytest {
     public void mytest()
     {
-        System.out.println("namaste telangana ");
+        System.out.println("namaste telangana,this is web hook ");
     }
     public static void main(String[] args) {
         Mytest m=new Mytest();
