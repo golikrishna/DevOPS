a702674b661adf6acce6b1e2d98b9b5b163b1dc5
golikrishna <49666896+golikrishna@users.noreply.github.com>
2019-04-30 19:40:38 +0530

    Update Mytest.java

--- src/main/java/Mytest.java
+++ src/main/java/Mytest.java
@@ -1,7 +1,7 @@
 public class Mytest {
     public void mytest()
     {
-        System.out.println("namaste telangana how are you testing you");
+        System.out.println("namaste telangana ");
     }
     public static void main(String[] args) {
         Mytest m=new Mytest();
