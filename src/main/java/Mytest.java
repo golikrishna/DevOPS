086338929fa4e5cabd6c62e1e26d75d413454461
krishna <krishna.goli@cgi.com>
2019-04-30 19:23:34 +0530

    testproject

--- src/main/java/Mytest.java
+++ src/main/java/Mytest.java
@@ -1,7 +1,7 @@
 public class Mytest {
     public void mytest()
     {
-        System.out.println("namaste telangana");
+        System.out.println("namaste telangana how are you ");
     }
     public static void main(String[] args) {
         Mytest m=new Mytest();
